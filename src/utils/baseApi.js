77d71aede5d2d80db68a64c79340bd91f7b7c660
lsy <598348605@qq.com>
2019-07-06 22:59:52 +0800

    replace host api

--- src/utils/baseApi.js
+++ src/utils/baseApi.js
@@ -1,9 +1,12 @@
 const baseUrl = location.hostname
 let baseApi
 switch (baseUrl) {
-  case 'www.lebronjames.top' || 'lebronjames.top':
+  case 'www.lebronjames.top':
     baseApi = 'https://www.lebronjames.top/api/'
     break
+  case 'lebronjames.top':
+    baseApi = 'https://www.lebronjames.top/api/'
+	break
   case 'localhost':
   default:
     baseApi = 'http://localhost:7654/api/'
